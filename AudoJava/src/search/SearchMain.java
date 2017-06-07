c828fe193bef62594a0da23b721106bfecacd27c
ZSL <ZSL@ZSL-PC>
2017-06-07 16:39:02 +0800

    搜索本地电脑上文档的位置

--- AudoJava/src/search/SearchMain.java
+++ AudoJava/src/search/SearchMain.java
@@ -11,21 +11,28 @@ import java.util.List;
  * @time 2016年12月13日
  */
 public class SearchMain {
-	private static String con="yiibai";//搜索条件
+//	private static String con="Java 并发编程实战.pdf";//搜索条件
+	private static String con="game";//搜索条件
 	private static String noInclude="";//不包括
-	private static String range="F:\\maven";//范围(目录)
+	private static String[] range=new String[]{"D:","E:","F:","C:"};//范围(目录)
 	
 	public static void main(String[] args) {
-		
+		for (String str : range) {
+			getFiles(new File(str));
+		}
 	}
 	
 	public static void getFiles(File file){
-		List<File> files=new ArrayList<File>();
+		File[] files=null;
+		if(file.getName().contains(con)){
+			System.out.println(file.getAbsolutePath());
+		}
 		if(file.isDirectory()){
-			files=Arrays.asList(file.listFiles());
-		}else{
-			if(file.getName().contains(con)){
-				System.out.println(file.getAbsolutePath());
+			files=file.listFiles();
+			if(files!=null){
+				for (File f : files) {
+					getFiles(f);
+				}
 			}
 		}
 	}
